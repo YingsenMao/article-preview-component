4b0e1a5f68de4f86d35b01ac709380c7aa1dbeb7
yingsenm <yingsenm@rpmhealthcare.com>
2025-07-07 20:52:10 -0400

    added html files

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 const viewportWidth = window.innerWidth;
-const share = document.querySelector(".share-btn");
-const popup = document.querySelector(".author.after-share");
-const exist = document.querySelector(".author.before-share");
+const shareBtn = document.querySelector(".share-btn");
+const shareMobileBtn = document.querySelector(".share-btn.mobile-btn");
+const authorDescriptoin = document.querySelector(".description-author");
+const shareDescription = document.querySelector(".description-share");
 
 console.log(viewportWidth);
-share.addEventListener("click", () => {
-
+shareBtn.addEventListener("click", () => {
   if (viewportWidth < 768) {
-    popup.style.display = "flex";
-    exist.style.display = "none";
-    share.classList.add('share-btn-share');
+    authorDescriptoin.style.display = "none";
+    shareDescription.style.display = "flex";
+    shareMobileBtn.style.display = "flex";
   } else {
-
-
-
-}});
+    shareDescription.classList.add("description-share-desktop");
+  }
+});
 
 document.addEventListener("click", (e) => {
-  if (!popup.contains(e.target) && !share.contains(e.target)) {
-    popup.style.display = "none";
-    exist.style.display = "flex";
-    share.classList.remove('share-btn-share');
+  if (!shareDescription.contains(e.target) && !shareBtn.contains(e.target)) {
+    if (viewportWidth < 768) {
+      shareDescription.style.display = "none";
+      authorDescriptoin.style.display = "flex";
+      shareBtn.classList.remove("share-btn-mobile");
+    } else {
+      shareDescription.classList.remove("description-share-desktop");
+    }
   }
 });
